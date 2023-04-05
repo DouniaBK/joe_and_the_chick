04e1ccfb22bf1e757ee5d7bd039dac4c06e3cc19
DouniaBK <douniazedbacha@gmail.com>
2023-04-05 14:12:30 +0000

    Initial commit

--- run.py
+++ run.py
@@ -1,3 +1,16 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import curses
+from curses import textpad
+
+# create the textpad rectangle where the field goes
+
+
+def main(stdscr):
+    curses.curs_set(0)
+    sh, sw = stdscr.getmaxyx()
+    box = [[3, 3], [sh-3, sw-3]]
+    textpad.rectangle(stdscr, box[0][0], box[0][1], box[1][0], box[1][1])
+    stdscr.getch()
+
+
+    
+curses.wrapper(main)
