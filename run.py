834793324ca1115d8f5a4e9b6ecd1cb7f9ad9a9d
DouniaBK <douniazedbacha@gmail.com>
2023-04-05 14:22:06 +0000

    Draw snakes body

--- run.py
+++ run.py
@@ -10,7 +10,14 @@ def main(stdscr):
     box = [[3, 3], [sh-3, sw-3]]
     textpad.rectangle(stdscr, box[0][0], box[0][1], box[1][0], box[1][1])
     stdscr.getch()
-
-
+    # set the snake's 3 body parts
+    snake = [[sh//2, sw//2+1], [sh//2, sh//2], [sh//2, sw//2-1]]
+    direction = curses.KEY_RIGHT
+    # draw snake body
+    for y, x in snake:
+        stdscr.addstr(y, x, '🟰')
     
+    
+
+
 curses.wrapper(main)
