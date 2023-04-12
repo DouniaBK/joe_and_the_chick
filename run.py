72b43a02e2096de64e2197b61c08fe0996ff0696
DouniaBK <douniazedbacha@gmail.com>
2023-04-12 12:53:05 +0000

    Copy class game implementation from game.py to run.py

--- run.py
+++ run.py
@@ -4,207 +4,204 @@ import random
 import copy
 
 
-OPPOSITE_DIRECTION_DICT = {
-    curses.KEY_UP: curses.KEY_DOWN,
-    curses.KEY_DOWN: curses.KEY_UP,
-    curses.KEY_RIGHT: curses.KEY_LEFT,
-    curses.KEY_LEFT: curses.KEY_RIGHT
-}
+class Game:
+    def __init__(self, stdscr):
+        curses.noecho()
+        self.stdscr = stdscr  # Standardscreen parameter returned by curses
+        self.snake = []
+        self.chick = []
+        self.box = []
+        self.score = 0
+        self.menu = ['Home', 'Play', 'Settings', 'Exit']
 
-DIRECTIONS_LIST = [curses.KEY_RIGHT,
-                   curses.KEY_LEFT, curses.KEY_DOWN, curses.KEY_UP]
+    """-------------------- MENU -----------------------"""
+    def print_menu(self, selected_row_idx):
+        self.stdscr.clear()
+        h, w = self.stdscr.getmaxyx()
+        for idx, row in enumerate(self.menu):
+            x = w//2 - len(row)//2
+            y = h//2 - len(self.menu)//2 + idx
+            if idx == selected_row_idx:
+                self.stdscr.attron(curses.color_pair(1))
+                self.stdscr.addstr(y, x, row)
+                self.stdscr.attroff(curses.color_pair(1))
+            else:
+                self.stdscr.addstr(y, x, row)
+        self.stdscr.refresh()
 
+    def print_center(self, text):
+        self.stdscr.clear()
+        h, w = self.stdscr.getmaxyx()
+        x = w//2 - len(text)//2
+        y = h//2
+        self.stdscr.addstr(y, x, text)
+        self.stdscr.refresh()
 
+    def menu_main(self):
 
-"""-------------------- MENU -----------------------"""
-menu = ['Home', 'Play', 'Settings', 'Exit']
+        curses.curs_set(0)
+        curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_CYAN)
 
-def print_menu(stdscr, selected_row_idx):
-    stdscr.clear()
-    h, w = stdscr.getmaxyx()
-    for idx, row in enumerate(menu):
-        x = w//2 - len(row)//2
-        y = h//2 - len(menu)//2 + idx
-        if idx == selected_row_idx:
-            stdscr.attron(curses.color_pair(1))
-            stdscr.addstr(y, x, row)
-            stdscr.attroff(curses.color_pair(1))
-        else:
-            stdscr.addstr(y, x, row)
-    stdscr.refresh()
+        current_row = 0
+        self.print_menu(current_row)
 
+        while 1:
+            key = self.stdscr.getch()
 
-def print_center(stdscr, text):
-    stdscr.clear()
-    h, w = stdscr.getmaxyx()
-    x = w//2 - len(text)//2
-    y = h//2
-    stdscr.addstr(y, x, text)
-    stdscr.refresh()
+            if key == curses.KEY_UP and current_row > 0:
+                current_row -= 1
+                
+            elif key == curses.KEY_DOWN and current_row < len(self.menu)-1:
+                current_row += 1
+            elif (key == curses.KEY_ENTER) or (key in [10, 13]):
 
+                if self.menu[current_row] == 'Play':
+                    return True
 
-def menu_main(stdscr):
+                if self.menu[current_row] == 'Exit':
+                    return False
 
-    curses.curs_set(0)
-    curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_CYAN)
+            self.print_menu(current_row)
+        
+    """-------------------- Game -----------------------"""
+    """
+    find the food coordinates making sure it appears inside the box
+    but not on the body of the snake 
+    """
+    def food_coord(self):
+        coords_chick = None
 
-    current_row = 0
-    print_menu(stdscr, current_row)
+        while coords_chick is None:
+            coords_chick = [random.randint(self.box[0][0]+1, self.box[1][0]-1), random.randint(self.box[0][1]+1, self.box[1][1]-1)]
+            if coords_chick in self.snake:
+                coords_chick = None
+        return coords_chick
 
-    while 1:
-        key = stdscr.getch()
+    # print score in the middle of the box
+    def print_score(self):
+        sh, sw = self.stdscr.getmaxyx()
+        score_display = "Score: {}".format(self.score)
+        self.stdscr.addstr(1, sw//2-len(score_display)//2, score_display)
+        self.stdscr.refresh()
 
-        if key == curses.KEY_UP and current_row > 0:
-            current_row -= 1
-            
-        elif key == curses.KEY_DOWN and current_row < len(menu)-1:
-            current_row += 1
-        elif (key == curses.KEY_ENTER) or (key in [10, 13]):
+    """
+    Determine if the snake ate the chick and return true if so
+    The terminal draws the chick one cell to the left
+    due to the emoji size. This visual offset is
+    compensated for here by extending the radius of possible hits
+    """
 
-            if menu[current_row] == 'Play':
-                return True
-
-            if menu[current_row] == 'Exit':
-                return False
-
-        print_menu(stdscr, current_row)
+    def snake_ate_chick(self, coords_snake_head, coords_chick):
+        chick_with_offset = copy.deepcopy(coords_chick)
+        chick_with_offset[1] = chick_with_offset[1] + 1
+        return (coords_snake_head == chick_with_offset) or (coords_snake_head == coords_chick)
     
+    def run(self):
+        # ----- Show Menu ----------
+        ret_val = self.menu_main()
+        # menu_main returns true or false, to continue or quit the game respectively
+        if not ret_val:
+            return
+        # ----- Start Game ----------
+        # set up curses
+        curses.curs_set(0)
+        self.stdscr.erase()
+        self.stdscr.nodelay(1)
+        self.stdscr.timeout(350)
+
+        # create the textpad rectangle where the field goes
+        sh, sw = self.stdscr.getmaxyx()
+        self.box = [[3, 3], [sh-3, sw-3]]
+        textpad.rectangle(self.stdscr, self.box[0][0], self.box[0][1], self.box[1][0], self.box[1][1])
+        self.stdscr.getch()
+
+        # set the snake's 3 body parts
+        self.snake = [[sh//2, sw//2+1], [sh//2, sh//2], [sh//2, sw//2-1]]
+        direction = curses.KEY_RIGHT
+
+        # draw snake's body with a character emoji
+        for y, x in self.snake:
+            self.stdscr.addstr(y, x, '▓')
+
+        # create the chick with an emoji
+        self.chick = self.food_coord()
+        self.stdscr.addstr(self.chick[0], self.chick[1], '🐤')
+
+        # print score
+        self.score = 0
+        self.print_score()
+
+        while 1:
+            # everytime the snake moves, anew head is created
+            # ask the user to press a key
+            key = self.stdscr.getch()
+
+            if key in [curses.KEY_RIGHT, curses.KEY_LEFT, curses.KEY_UP,
+                    curses.KEY_DOWN]:
+                direction = key
+
+            head = self.snake[0]
+
+            if direction == curses.KEY_RIGHT:
+                new_head = [head[0], head[1]+1]
+            elif direction == curses.KEY_LEFT:
+                new_head = [head[0], head[1]-1]
+            elif direction == curses.KEY_UP:
+                new_head = [head[0]-1, head[1]]
+            elif direction == curses.KEY_DOWN:
+                new_head = [head[0]+1, head[1]]
+
+            # insert a new head
+            self.stdscr.addstr(new_head[0], new_head[1], '▓')
+            self.snake.insert(0, new_head)
+
+            # increment the score if snake catches the chick
+            # display a new chick after the last one is eaten
+            # and increase the lenght of the snake
+            eaten = self.snake_ate_chick(self.snake[0], self.chick)
+            if eaten:
+                # increment score
+                self.score += 1
+                self.print_score()
+                # display a new chick everytime the snake eats the last one
+                self.chick = self.food_coord()
+
+                self.stdscr.addstr(self.chick[0], self.chick[1], '🐤')
+                # increase speed of the game
+                self.stdscr.timeout(100 - (len(self.snake)//3) % 90)
+            else:
+                # to mimic motion the last part of the head has to be removed and
+                # replaced by a space
+                self.stdscr.addstr(self.snake[-1][0], self.snake[-1][1], ' ')
+                self.snake.pop()
+            # rules of the game
+            # if snake crashes agaist the border or bites itself
+            # the game is over.
+            if (self.snake[0][0] in [self.box[0][0], self.box[1][0]] or
+                self.snake[0][1] in [self.box[0][1], self.box[1][1]] or
+                    self.snake[0] in self.snake[1:]):
+                msg = "Game Over!"
+                self.stdscr.addstr(sh//2, sw//2-len(msg)//2, msg)
+                self.stdscr.nodelay(0)
+                self.stdscr.getch()
+                break
+
+            self.stdscr.refresh()
+            # key = self.stdscr.getch()
 
 
-"""-------------------- GAME -----------------------"""
-
-"""
-find the food coordinates making sure it appears inside the box
-but not on the body of the snake 
-"""
-
-def food_coord(snake, box):
-    chick = None
-
-    while chick is None:
-        chick = [random.randint(box[0][0]+1, box[1][0]-1),
-                 random.randint(box[0][1]+1, box[1][1]-1)]
-        if chick in snake:
-            chick = None
-    return chick
+def main(stdscr): 
+    game = Game(stdscr)
+    game.run()
+    curses.endwin()
 
 
-# print score in the middle of the box
-def print_score(stdscr, score):
-    sh, sw = stdscr.getmaxyx()
-    score_display = "Score: {}".format(score)
-    stdscr.addstr(1, sw//2-len(score_display)//2, score_display)
-    stdscr.refresh()
-
-
-"""
-Determine if the snake ate the chick and return true if so
-The terminal draws the chick one cell to the left
-due to the emoji size. This visual offset is
-compensated for here by extending the radius of possible hits
-"""
-
-
-def snake_ate_chick(coords_snake_head, coords_chick):
-    chick_with_offset = copy.deepcopy(coords_chick)
-    chick_with_offset[1] = chick_with_offset[1] + 1
-    return (coords_snake_head == chick_with_offset) or (coords_snake_head == coords_chick)
-
-
-def main(stdscr):
-    # ----- Show Menu ----------
-    ret_val = menu_main(stdscr)
-    # menu_main returns true or false, to continue or quit the game respetively
-    if not ret_val:
-        return
-
-    # ----- Start Game ----------
-    # set up curses
-    curses.curs_set(0)
-    curses.noecho()
-    stdscr.erase()
-    stdscr.nodelay(1)
-    stdscr.timeout(350)
-
-    # create the textpad rectangle where the field goes
-    sh, sw = stdscr.getmaxyx()
-    box = [[3, 3], [sh-3, sw-3]]
-    textpad.rectangle(stdscr, box[0][0], box[0][1], box[1][0], box[1][1])
-    stdscr.getch()
-
-    # set the snake's 3 body parts
-    snake = [[sh//2, sw//2+1], [sh//2, sh//2], [sh//2, sw//2-1]]
-    direction = curses.KEY_RIGHT
-
-    # draw snake's body with a character emoji
-    for y, x in snake:
-        stdscr.addstr(y, x, '▓')
-
-    # create the chick with an emoji
-    chick = food_coord(snake, box)
-    stdscr.addstr(chick[0], chick[1], '🐤')
-
-    # print score
-    score = 0
-    print_score(stdscr, score)
-
-    while 1:
-        # everytime the snake moves, anew head is created
-        # ask the user to press a key
-        key = stdscr.getch()
-
-        if key in [curses.KEY_RIGHT, curses.KEY_LEFT, curses.KEY_UP,
-                   curses.KEY_DOWN]:
-            direction = key
-
-        head = snake[0]
-
-        if direction == curses.KEY_RIGHT:
-            new_head = [head[0], head[1]+1]
-        elif direction == curses.KEY_LEFT:
-            new_head = [head[0], head[1]-1]
-        elif direction == curses.KEY_UP:
-            new_head = [head[0]-1, head[1]]
-        elif direction == curses.KEY_DOWN:
-            new_head = [head[0]+1, head[1]]
-
-        # insert a new head
-        stdscr.addstr(new_head[0], new_head[1], '▓')
-        snake.insert(0, new_head)
-
-        # increment the score if snake catches the chick
-        # display a new chick after the last one is eaten
-        # and increase the lenght of the snake
-        eaten = snake_ate_chick(snake[0], chick)
-        if eaten:
-            # increment score
-            score += 1
-            print_score(stdscr, score)
-            # display a new chick everytime the snake eats the last one
-            chick = food_coord(snake, box)
-
-            stdscr.addstr(chick[0], chick[1], '🐤')
-            # increase speed of the game
-            stdscr.timeout(100 - (len(snake)//3) % 90)
-        else:
-            # to mimic motion the last part of the head has to be removed and
-            # replaced by a space
-            stdscr.addstr(snake[-1][0], snake[-1][1], ' ')
-            snake.pop()
-        # rules of the game
-        # if snake crashes agaist the border or bites itself
-        # the game is over.
-        if (snake[0][0] in [box[0][0], box[1][0]] or
-            snake[0][1] in [box[0][1], box[1][1]] or
-                snake[0] in snake[1:]):
-            msg = "Game Over!"
-            stdscr.addstr(sh//2, sw//2-len(msg)//2, msg)
-            stdscr.nodelay(0)
-            stdscr.getch()
-            break
-
-        stdscr.refresh()
-        # key = stdscr.getch()
-
-
-curses.wrapper(main)
+# Run the game if this is main
+if __name__ == "__main__":
+    try:
+        # start main program loop
+        curses.wrapper(main)
+            
+    # quit curses and print exception if there was an error
+    except Exception:
+        print("Exception")
